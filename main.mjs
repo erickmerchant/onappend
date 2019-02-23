2137c1e033a5e048d22b2eaf736b4da1510121a4
Erick Merchant <erickmerchant@gmail.com>
2019-02-23 10:28:27 -0800

    Initial Commit

---
+++ main.mjs
@@ -0,0 +1,43 @@
+/* global MutationObserver */
+
+const call = (target, event) => {
+  if (target[event]) {
+    target[event].call(target)
+
+    target[event] = null
+  }
+}
+
+const traverse = (target, event) => {
+  call(target, event)
+
+  for (const child of target.childNodes) {
+    traverse(child, event)
+  }
+}
+
+const config = {attributes: true, childList: true, characterData: true, subtree: true}
+
+const callback = (mutations) => {
+  for (const mutation of mutations) {
+    const target = mutation.target
+
+    call(target, 'onappend')
+
+    for (const added of mutation.addedNodes) {
+      traverse(added, 'onappend')
+    }
+
+    for (const removed of mutation.removedNodes) {
+      traverse(removed, 'onremove')
+    }
+  }
+}
+
+export default (target) => {
+  const observer = new MutationObserver(callback)
+
+  observer.observe(target, config)
+
+  return observer.disconnect
+}
